a199230b37d0f750429b8a0668bc3f0f3b027564
Hansen Liang <thehansenliang@gmail.com>
2020-11-21 16:17:54 -0500

    POC widget working and styled.

---
+++ ios/widget/ColorExtension.swift
@@ -0,0 +1,16 @@
+//
+//  ColorExtension.swift
+//  widgetExtension
+//
+//  Created by Hansen Liang on 11/15/20.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let HeadlineTextColor = Color("HeadlineTextColor")
+    static let CaptionTextColor = Color("CaptionTextColor")
+    static let WidgetBackgroundColor = Color("WidgetBackgroundColor")
+    
+}
